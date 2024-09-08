c60c7eeb40da89dac60961dc86d57c586edf0711
Khur0o <121981931+Khur0o@users.noreply.github.com>
2024-09-08 18:13:50 +0800

    upload

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,24 @@
-function switchForm(type) {
+function switchForm(formType) {
     const urlForm = document.getElementById('url-form');
     const fileForm = document.getElementById('file-form');
+    
+    const urlLink = document.querySelector('a[href="#"][onclick*="url"]');
+    const fileLink = document.querySelector('a[href="#"][onclick*="file"]');
 
-    if (type === 'url') {
+    // Show or hide forms based on selected option
+    if (formType === 'url') {
         urlForm.style.display = 'block';
         fileForm.style.display = 'none';
-    } else if (type === 'file') {
+
+        // Set the active class to URL
+        urlLink.classList.add('active');
+        fileLink.classList.remove('active');
+    } else if (formType === 'file') {
         urlForm.style.display = 'none';
         fileForm.style.display = 'block';
+
+        // Set the active class to File
+        fileLink.classList.add('active');
+        urlLink.classList.remove('active');
     }
 }
